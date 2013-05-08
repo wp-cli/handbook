d5b1163bb65df3198d0a8b747bfc48a37d0e48d6
scribu <mail@scribu.net>
2013-05-08 20:47:27 +0300

    attempt to find appropriate PHP CLI

--- installer.sh
+++ installer.sh
@@ -2,18 +2,50 @@
 
 set -e
 
-# check that PHP is available
-command -v php > /dev/null || {
-	echo "can't find PHP binary" >&2
+find_php() {
+	AMP_PATHS=<<EOB
+/Applications/MAMP/bin/php/php5.3*/bin/php
+/Applications/MAMP/bin/php5*/bin/php
+/Applications/MAMP/bin/php/php5.[34]*/bin/php
+/Applications/xampp/xamppfiles/bin/php;
+/opt/lampp/bin/php
+EOB
+
+	# Special case for *AMP installers, since they normally don't set themselves
+	# as the default cli php out of the box.
+	for amp_php in $AMP_PATHS; do
+		if [ -x $amp_php ]; then
+			echo $amp_php
+			exit
+		fi
+	done
+
+	for cmd in php php-cli; do
+		command -v $cmd > /dev/null && {
+			echo $cmd
+			exit
+		}
+	done
+
 	exit 1
 }
+
+# Find a PHP binary
+PHP=`find_php`
+if [ $? -eq 0 ]; then
+	if [ "$PHP" != 'php' ]; then
+		echo "using non-default PHP CLI: $php"
+	fi
+else
+	read -p "path to PHP CLI: " PHP
+fi
 
 mkdir -p ~/.composer
 cd ~/.composer
 
 # install Composer
 if [ ! -x composer.phar ]; then
-	curl -sS https://getcomposer.org/installer | php
+	curl -sS https://getcomposer.org/installer | $PHP
 fi
 
 # set up global composer.json file
@@ -31,7 +63,7 @@ if [ ! -f composer.json ]; then
 EOB
 fi
 
-COMPOSER="php composer.phar"
+COMPOSER="$PHP composer.phar"
 
 # install wp-cli
 $COMPOSER require wp-cli/wp-cli=dev-master
