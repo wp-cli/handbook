4993e44a62f1ff7a2b2626b1e4a880657f862b6e
smhmic <smhmic@gmail.com>
2013-09-09 02:12:45 -0400

    detect if Composer returned unsuccessful exit code
    missed removing extra echo in last commit

--- installer.sh
+++ installer.sh
@@ -72,12 +72,11 @@ command -v bin/wp > /dev/null || {
 	echo "Installing WP-CLI in $INSTALL_DIR"
 	echo "-----------------"
 	$COMPOSER require --prefer-source wp-cli/wp-cli="$VERSION"
-	echo "$OUTPUT"
-	echo
 	if [ $? -ne 0 ]; then
 	   echo "WP-CLI was not successfully installed."
 	   exit 1
 	else
+	   echo
 	   echo "WP-CLI files have been successfully installed."
    	fi
    fi
