3ca0ed1f5f8891b13398b67f7b820e71ad74541f
miya0001 <miya@wpist.me>
2016-06-30 18:12:28 +0900

    define languages in the _config.yml

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,16 +1,5 @@
 ( function( $ ) {
-  var select = $( '<select />' );
-  $( select ).append( '<option>Change Language</option>' );
-  $( '.cf .container' ).append( select );
-  $.getJSON( '/i18n.json', function( data ) {
-    $( data ).each( function() {
-      var option = $( '<option />' );
-      $( option ).text( this['label'] );
-      $( option ).val( this['path'] );
-      $( select ).append( option );
-    } );
-  } );
-  $( select ).on( 'change', function() {
+  $( 'select.languages' ).on( 'change', function() {
     if ( $( this ).val() ) {
       location.href = $( this ).val();
     }
